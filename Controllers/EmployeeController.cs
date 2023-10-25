c929eb016a55c09542e2d5af72f0f49e4b648649
StartlordR <rohitsurvase4363@gmail.com>
2023-10-25 21:26:11 +0530

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Demo2MVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
